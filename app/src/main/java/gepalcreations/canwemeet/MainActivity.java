b5f39a574960660cdcd8e8a0256f19c3b5fa5f15
vasilis <vspallas@gmail.com>
2015-05-07 04:32:05 +0300

    fixed current time

--- app/src/main/java/gepalcreations/canwemeet/MainActivity.java
+++ app/src/main/java/gepalcreations/canwemeet/MainActivity.java
@@ -10,11 +10,13 @@ import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.util.DisplayMetrics;
+import android.util.Log;
 import android.view.Display;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import java.io.BufferedInputStream;
 import java.io.IOException;
@@ -24,10 +26,10 @@ public class MainActivity extends Activity {
 
     private LinearLayout timeLinearLayout;
     private View line;
-	private ImageView testImage, testImage2, testImage3, testImage4;
+    private ImageView testImage, testImage2, testImage3, testImage4;
     Context context = this;
 
-	int timeZone = 2; //for testing
+    int timeZone = 2; //for testing
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,114 +40,82 @@ public class MainActivity extends Activity {
         Clock mClock = new Clock();
         int currentMinutes = mClock.getMinutes();
         int currentHours = mClock.getHours();
-		int currentTimeZone = mClock.getTimeZone();
+        int currentTimeZone = mClock.getTimeZone();
 
-		//Log.e("hours", String.valueOf(currentHours));
-		int height = getScreenHeight() - getDensityPixelToRemove(context);
+        //Log.e("hours", String.valueOf(currentHours));
+        int height = getScreenHeight() - getDensityPixelToRemove(context);
 
-		//Log.e("Screen Height",String.valueOf(getScreenHeight()));
-		//Log.e("Screen Density",String.valueOf(getDensityPixelToRemove(context)));
-		//Log.e("Final Height",String.valueOf(height));
+        //Log.e("Screen Height",String.valueOf(getScreenHeight()));
+        //Log.e("Screen Density",String.valueOf(getDensityPixelToRemove(context)));
+        //Log.e("Final Height",String.valueOf(height));
 
         //Loading Layout
-        //timeLinearLayout = (LinearLayout) findViewById(R.id.time_linear_layout);
-		//timeLinearLayout.setOrientation(LinearLayout.VERTICAL);
+        timeLinearLayout = (LinearLayout) findViewById(R.id.current_time_linear);
+        timeLinearLayout.setOrientation(LinearLayout.VERTICAL);
         //Loading Line
         line = findViewById(R.id.line);
-		testImage = (ImageView) findViewById(R.id.testimage);
-		testImage2 = (ImageView) findViewById(R.id.testimage2);
-		testImage3 = (ImageView) findViewById(R.id.testimage3);
-		testImage4 = (ImageView) findViewById(R.id.testimage4);
+        //testImage = (ImageView) findViewById(R.id.testimage);
+        // testImage2 = (ImageView) findViewById(R.id.testimage2);
+        //testImage3 = (ImageView) findViewById(R.id.testimage3);
+        // testImage4 = (ImageView) findViewById(R.id.testimage4);
 
-		testImage.setTranslationY(250);
-		testImage.setTranslationX(250);
-		testImage2.setTranslationY(450);
-		testImage2.setTranslationX(250);
-		testImage3.setTranslationY(780);
-		testImage3.setTranslationX(250);
-		testImage4.setTranslationY(1270);
-		testImage4.setTranslationX(250);
-
-
-
+//        testImage.setTranslationY(250);
+        //     testImage.setTranslationX(250);
+        //     testImage2.setTranslationY(450);
+        //     testImage2.setTranslationX(250);
+        //     testImage3.setTranslationY(780);
+        //     testImage3.setTranslationX(250);
+        //     testImage4.setTranslationY(1270);
+        //      testImage4.setTranslationX(250);
 
 
         float timeCalculation = getTimeCalculation(currentHours, currentMinutes, height);
         line.setTranslationY(timeCalculation);
 
         //checking if we are in the same time zone to do other logic.
-        int timeZoneCheck = compareSameTimeZone(timeZone,currentTimeZone);
+        int timeZoneCheck = compareSameTimeZone(timeZone, currentTimeZone);
         if (timeZoneCheck == 1) {
             timeZone = 0;
         }
 
-        //loadImages(timeZone);
-		//loadImagesFromXML(timeZone);
+        loadImagesFromXML(timeZone);
     }
 
     ViewHolder holder;
 
-	public void loadImagesFromXML(int timeZone) {
-		View [] arrayView =
-				{findViewById(R.id.hour_1),findViewById(R.id.hour_2), findViewById(R.id.hour_3), findViewById(R.id.hour_4),
-						findViewById(R.id.hour_5),findViewById(R.id.hour_6), findViewById(R.id.hour_7), findViewById(R.id.hour_8),
-						findViewById(R.id.hour_9),findViewById(R.id.hour_10), findViewById(R.id.hour_11), findViewById(R.id.hour_12),
-						findViewById(R.id.hour_13),findViewById(R.id.hour_14), findViewById(R.id.hour_15), findViewById(R.id.hour_16),
-						findViewById(R.id.hour_17),findViewById(R.id.hour_18), findViewById(R.id.hour_19), findViewById(R.id.hour_20),
-						findViewById(R.id.hour_21),findViewById(R.id.hour_22), findViewById(R.id.hour_23), findViewById(R.id.hour_24)
-				};
-		String imageName;
-		//checking and correcting for negative value
-		if (timeZone < 0) {
-			timeZone = timeZone * (-1);
-		}
-
-		for (int i = 0; i < 25; i++) {
-			int indexOfFiles = (i + timeZone) % 25;
-			View view = arrayView[indexOfFiles];
-			view.bringToFront();
-			//LayoutInflater inflater = getLayoutInflater();
-			//holder = new ViewHolder();
-			//holder.image = (ImageView) v.findViewById(R.id.time_image);
-			/*if (indexOfFiles != 0) {
-				imageName = "time24h" + indexOfFiles;
-				try {
-					holder.image.setImageDrawable(getAssetImage(this, imageName));
-				} catch (IOException e) {
-					e.printStackTrace();
-				}
-				v.setTag(holder);
-				timeLinearLayout.addView(v);
-			}*/
-		}
-	}
-    public void loadImages(int timeZone) {
-
-        String imageName;
+    public void loadImagesFromXML(int timeZone) {
         //checking and correcting for negative value
         if (timeZone < 0) {
             timeZone = timeZone * (-1);
         }
 
         for (int i = 0; i < 25; i++) {
-            int indexOfFiles = (i + timeZone) % 25;
+            int indexOfFiles = ((i + 1) + timeZone) % 25;
             LayoutInflater inflater = getLayoutInflater();
-            View v = inflater.inflate(R.layout.time_layout, timeLinearLayout, false);
+            View v = inflater.inflate(R.layout.times_layout, timeLinearLayout, false);
+
             holder = new ViewHolder();
-            holder.image = (ImageView) v.findViewById(R.id.time_image);
             if (indexOfFiles != 0) {
-                imageName = "time24h" + indexOfFiles;
-                try {
-                    holder.image.setImageDrawable(getAssetImage(this, imageName));
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
+
+                holder.hour = (TextView) v.findViewById(R.id.hours);
+                holder.hour.setText(String.valueOf(indexOfFiles));
+                LinearLayout.LayoutParams hourParams = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.MATCH_PARENT, 1.0f);
+                if (indexOfFiles >= 1 && indexOfFiles <= 7)
+                    holder.hour.setBackgroundColor(getResources().getColor(R.color.md_purple_500));
+                else if (indexOfFiles >= 8 && indexOfFiles <= 9)
+                    holder.hour.setBackgroundColor(getResources().getColor(R.color.md_teal_500));
+                else if (indexOfFiles >= 10 && indexOfFiles <= 17)
+                    holder.hour.setBackgroundColor(getResources().getColor(R.color.md_amber_500));
+                else holder.hour.setBackgroundColor(getResources().getColor(R.color.md_brown_500));
+
+                holder.hour.setPaddingRelative(10, 0, 0, 0);
+
                 v.setTag(holder);
-                timeLinearLayout.addView(v);
+                timeLinearLayout.addView(v, hourParams);
             }
+            Log.i("index", String.valueOf(indexOfFiles));
         }
     }
-
 
     public static Drawable getAssetImage(Context context, String filename) throws IOException {
         AssetManager assets = context.getResources().getAssets();
@@ -154,56 +124,56 @@ public class MainActivity extends Activity {
         return new BitmapDrawable(context.getResources(), bitmap);
     }
 
-	private static int getDensityPixelToRemove (Context context) {
+    private static int getDensityPixelToRemove(Context context) {
 
-		float density = context.getResources().getDisplayMetrics().density;
-		//action bar 48dp and status bar 20dp
-		if (density >= 4.0) {
-			return 272;//48+20*3
-		}
-		if (density >= 3.0) {
-			return 204;
-		}
-		if (density >= 2.0) {
-			return 136;
-		}
-		if (density >= 1.5) {
-			return 102;
-		}
-		return 68;
-	}
+        float density = context.getResources().getDisplayMetrics().density;
+        //action bar 48dp and status bar 20dp
+        if (density >= 4.0) {
+            return 272;//48+20*3
+        }
+        if (density >= 3.0) {
+            return 204;
+        }
+        if (density >= 2.0) {
+            return 136;
+        }
+        if (density >= 1.5) {
+            return 102;
+        }
+        return 68;
+    }
 
-	public int getScreenHeight(){
+    public int getScreenHeight() {
 
-		Display display = getWindowManager().getDefaultDisplay();
-		DisplayMetrics metrics = new DisplayMetrics();
-		getWindowManager().getDefaultDisplay().getMetrics(metrics);
+        Display display = getWindowManager().getDefaultDisplay();
+        DisplayMetrics metrics = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(metrics);
 
-		Point size = new Point();
-		display.getSize(size);
-		return size.y;
-	}
+        Point size = new Point();
+        display.getSize(size);
+        return size.y;
+    }
 
-	//Calculate time for line animation
-	private float getTimeCalculation(int currentHours, int currentMinutes, float height) {
-		float pixelPerHour = height / 25;
+    //Calculate time for line animation
+    private float getTimeCalculation(int currentHours, int currentMinutes, float height) {
+        float pixelPerHour = height / 25;
 
-		return currentHours * pixelPerHour + currentMinutes;
-	}
+        return currentHours * pixelPerHour + currentMinutes;
+    }
 
     //Method to check if we are in the same time zone as entered by user. If yes then passing the 0 index to the array so
     //it will display the same image.
     private int compareSameTimeZone(int timeZone, int currentTimeZone) {
 
-		int timeZoneIsSame = 0; //initialize to not in same zone
+        int timeZoneIsSame = 0; //initialize to not in same zone
         if (timeZone == currentTimeZone) {
-			timeZoneIsSame = 1; //in same time zone
+            timeZoneIsSame = 1; //in same time zone
         }
-		return timeZoneIsSame;
-	}
+        return timeZoneIsSame;
+    }
 
     private class ViewHolder {
-        ImageView image;
+        TextView hour;
     }
 
 }
